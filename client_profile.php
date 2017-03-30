ccc94ad6b8c513ed7a2ac4e48c0ee905f83dee60
fran <fran.blasco94@gmail.com>
2017-03-30 13:32:59 +0200

    Actualización que permite iniciar y cerrar sesión.

--- client_profile.php
+++ client_profile.php
@@ -1,19 +1,41 @@
 <?php
 //Los nombres deben ir en inglés
 //$Cliente vendrá de un archivo JSON.
-$string = file_get_contents("clientes_falsos.json");
-$json_a = json_decode($string, true);
-foreach ($json_a as $key => $value) {
-	if($_POST['user'] == $key){
-		$client = $_POST['user'];
-		break;
-	}
-	else{
-		$client =  $_POST['user']. ", no estás registrado." ;
-	}
 
-}
 // echo $client['name'];
+
+session_start();
+
+$logged = isset($_SESSION['user']);
+$logout = isset($_POST['logout']);
+
+if(isset($_POST['user']) && !$logout){
+	$string = file_get_contents("clientes_falsos.json");
+	$json_a = json_decode($string, true);
+	foreach ($json_a as $key => $value) {
+		if($_POST['user'] == $key){
+			$_SESSION['user'] = $client = $_POST['user'];
+			$logged = true;
+			break;
+		}
+		else{
+			$client =  $_POST['user']. ", no estás registrado." ;
+		}
+
+	}
+}
+
+if($logged && !$logout){
+	$client = $_SESSION['user'];
+}
+else{
+	session_destroy();
+	header('location:formClient.php');
+	die();
+}
+
+
+
 
 ?>
 
@@ -25,6 +47,9 @@ foreach ($json_a as $key => $value) {
 </head>
 <body>
 	<p>Hola, <?= $client?></p>
+	<form method="POST">
+		<input type="submit" name="logout" value="Cerrar sesión">
+	</form>
 	<table>
 		<!-- Aquí irían los datos del cliente. -->
 	</table>
