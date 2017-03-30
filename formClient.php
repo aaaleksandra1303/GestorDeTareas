94301213b458b4cb0cc1390d5dfbc43db61013aa
Curso PHP <davidign1688@hotmail.com>
2017-03-30 12:07:07 +0200

    Form client to send page perfil

---
+++ formClient.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <form action="client_profile.php" method="POST">
+        User:<input type="text" name="user" placeholder="user">
+        <input type="submit" name="submit">
+    </form>
+</body>
+</html>
