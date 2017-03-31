80aeaf39718b4f5ea061807b82622a1a02c7d568
Davidfhk <davidign1688@hotmail.com>
2017-03-31 10:53:18 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 </head>
 <body>
 	<a href="formClient.php">Login de cliente</a><br>
-	<a href="formTechnician.php">Login de técnicos</a><br>
+	<a href="formTec.php">Login de técnicos</a><br>
 	<a href="formAdmin.php">Login del admin (no disponible actualmente)</a>
 </body>
 </html>
